5c652fa812295f1669e52d0d63a4c87faeeff823
MeasyCharlie <solankiu468@gmail.com>
2023-06-19 11:55:44 +0530

    add cart ,login and register

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const expressLayout = require('express-ejs-layouts');
 
 const PORT = process.env.PORT || 3000
 
+//set template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
 
 //Assets
 app.use(express.static('public'));
@@ -13,10 +17,18 @@ app.get('/', (req, res)=>{
     res.render('home');
 })
 
-//set template engine
-app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.get('/cart', (req,res)=>{
+    res.render('customers/cart');
+})
+
+
+app.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+app.get('/register', (req, res)=>{
+    res.render('auth/register')
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
