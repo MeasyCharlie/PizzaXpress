908be83f5b8729f3b6077cc1bfaef243e9d6ec75
MeasyCharlie <solankiu468@gmail.com>
2023-07-28 14:19:33 +0530

    passport fixed

--- app/config/passport.js
+++ app/config/passport.js
@@ -1,68 +1,45 @@
-const LocalStrategy = require('passport-local').Strategy
-const User = require('../models/user')
-const bcrypt = require('bcrypt')
-function init(passport) {
-    passport.use( new LocalStrategy({ usernameField: 'email' }, async (email, password, done) =>{
-        //login logic
-        //check if email exists
-        const user = await User.findOne({ email: email})
-        if(!user) {
-            return done(null, false, {message: 'No user with this email'})
-        }
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+// function init(passport) {
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      //login logic
+      //check if email exists
+      const user = await User.findOne({ email: email });
+      if (!user) {
+        return done(null, false, { message: "No user with this email" });
+      }
 
-        bcrypt.compare(password, user.password).then(match =>{
-            if(match) {
-                return done(null, user, { message: 'Logged in successfully'});
-            }
-            return done(null, false, { message: 'Wring username or password'})
-        }).catch(err => {
-            return done(null, false, {message: 'Something went wrong'});
+      bcrypt
+        .compare(password, user.password)
+        .then((match) => {
+          if (match) {
+            return done(null, user, { message: "Logged in successfully" });
+          }
+          return done(null, false, { message: "Wring username or password" });
         })
-
-
-        //user recieved when password match
-        passport.serializeUser((user, done) =>{
-            done(null, user._id)
-        })
-
-        // passport.deserializeUser( (id, done) => {
-        //     User.findById(id).then(user =>{
-        //         done( null,user);
-        //     })
-        // })
-        // passport.deserializeUser(function(id, done){
-        //     User.findById(id).then(user => {
-        //         // if(err){
-        //         //     console.log('Error in finding user --> Passport');
-        //         //     return done(err);
-        //         // }
-        //         return done(null, user);
-        //     })
-        // })
-        // passport.deserializeUser(function(user, done) {
-        //     process.nextTick(function() {
-        //       return done(null, user);
-        //     });
-        // });
-        // passport.deserializeUser --> User.findById(id).exec()
-        // .then(user => {
-        //   done(null, user);
-        // })
-        // .catch(err => {
-        //   done(err);
-        // });
- 
-
-        //accepted code
-        passport.deserializeUser(function(id, done) {
-            console.log('deserailize ', id) ;
-            User.findById(id).then(function (user) {
-                done(null, user);
-            }).catch(function (err) {
-                done(err, null, { message: 'User does not exist' });
-            });
+        .catch((err) => {
+          return done(null, false, { message: "Something went wrong" });
         });
-    }))
-}
+    }
+  )
+);
+// }
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id)
+    .then(function (user) {
+      done(null, user);
+    })
+    .catch(function (err) {
+      done(err, null, { message: "User does not exist" });
+    });
+});
 
-module.exports = init;
+module.exports = passport;
--- server.js
+++ server.js
@@ -1,55 +1,53 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const ejs = require('ejs')
-const path = require('path');
-const expressLayout = require('express-ejs-layouts');
-const PORT = process.env.PORT || 3000
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
+const PORT = process.env.PORT || 3000;
 
-const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('express-flash')
-const MongoDbStore = require('connect-mongo');
-const passport = require('passport');
-const { FALSE } = require('sass');
-const Emitter = require('events');
-
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+const MongoDbStore = require("connect-mongo");
+const passport = require("passport");
+const { FALSE } = require("sass");
+const Emitter = require("events");
 
 // //database Connection
 // const url = 'mongodb://localhost/PizzaExpress';
-mongoose.connect(process.env.MONGO_CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Database connected...');
-}).on('error', function(err) {
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .on("error", function (err) {
     console.log(err);
-})
+  });
 
-//session store
-// let mongoStore = new MongoDbStore({
-    //     mongooseConnection: connection,
-    //     collection: 'sessions'
-    // })
-    
-    //event emitter
-    
-    const eventEmitter = new Emitter(); 
-    app.set('eventEmitter', eventEmitter);
-    
-    //session config
-    app.use(session({
-        secret: process.env.COOKIE_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        store: MongoDbStore.create({
-            client: connection.getClient()
-        }),
-        cookie: {maxAge: 1000 * 60 * 60 * 24}
-    }));
-    
-    //passport config
-const passportInit = require('./app/config/passport');
-passportInit(passport);
+
+const eventEmitter = new Emitter();
+app.set("eventEmitter", eventEmitter);
+
+//session config
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoDbStore.create({
+      client: connection.getClient(),
+    }),
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+//passport config
+require("./app/config/passport");
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -57,49 +55,40 @@ app.use(passport.session());
 app.use(flash());
 
 //Assets
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //global middleware jisse ki session ki info layout.ejs tak ja sake
-app.use((req, res, next)=>{
-    
-    res.locals.session = req.session;
-    res.locals.user = req.user;
-    next()
-})
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  res.locals.user = req.user;
+  next();
+});
  
 //set template engine
 app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
+//(app) se function call ho rahi h jisse function chal jae web.js m
+require("./routes/web")(app);
 
-
-
-
-
-
-
-//(app) se function call ho rahi h jisse function chal jae web.js m 
-require('./routes/web')(app);
-
-const server = app.listen(PORT, ()=>{
-        console.log(`listening on port ${PORT}`);
-    })
-
+const server = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
 //socket
-const io = require('socket.io')(server);
-io.on('connection', (socket)=>{
-    //Join
-    // console.log(socket.id);
-    socket.on('join', (orderId)=>{
-        // console.log(orderId);
-        socket.join(orderId);
-    })
-})
+const io = require("socket.io")(server);
+io.on("connection", (socket) => {
+  //Join
+  // console.log(socket.id);
+  socket.on("join", (orderId) => {
+    // console.log(orderId);
+    socket.join(orderId);
+  });
+});
 
-eventEmitter.on('orderUpdated', (data)=>{
-    io.to(`order_${data.id}`).emit('orderUpdated', data);
-})
+eventEmitter.on("orderUpdated", (data) => {
+  io.to(`order_${data.id}`).emit("orderUpdated", data);
+});
